a563a502e83839cd1e2b2ba1e0f914620bea8eba
hoondal2 <ghdtjsrud20@gmail.com>
2022-07-18 10:04:25 +0900

    2022.07.18 DBConnection - mysql

---
+++ src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
@@ -0,0 +1,36 @@
+package com.fastcampus.ch3;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class DBConnectionTest2Test extends TestCase {
+    @Autowired
+    DataSource ds;
+
+    @Test
+    public void testMain() throws Exception{ // 테스트 시 언제나 public void, 메서드 이름은 상관없음
+//        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
+//        DataSource ds = ac.getBean(DataSource.class);
+
+        Connection conn = ds.getConnection(); // 데이터베이스의 연결을 얻는다.
+
+        System.out.println("conn = " + conn);
+        assertTrue(conn!=null); // 괄호 안의 조건식이 true이면, 테스트 성공, 아니면 실패
+
+
+    }
+
+}
